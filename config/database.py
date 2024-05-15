9243d85e153fbede518f4bfcaa30548ff90e1893
Martín Araya <mbozo@uc.cl>
2024-05-15 19:21:29 -0400

    testeeooo
    testeeeo

--- config/database.py
+++ config/database.py
@@ -2,7 +2,9 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 envPath = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(envPath,'.env'))
+rootPath = os.path.dirname(envPath)
+dotenvPath = os.path.join(rootPath, '.env')
+load_dotenv(dotenvPath)
 mongoUrl = os.getenv("MONGO_URL")
 client = MongoClient(mongoUrl)
 db = client.foomonkeys123
--- main.py
+++ main.py
@@ -7,5 +7,4 @@ app.include_router(router)
 
 if __name__ == '__main__':
     #insert_tests()
-
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
