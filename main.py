7bf84a6cbd0b0454d17b21159c2d076c5964ff3a
Martín Araya <mbozo@uc.cl>
2024-05-15 18:19:18 -0400

    NO FUNCIONA AYUDA

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ app.include_router(router)
 
 if __name__ == '__main__':
     #insert_tests()
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
