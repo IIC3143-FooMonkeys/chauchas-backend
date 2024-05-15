080b0781cc19d65e74ebeb95acb04aa23a243d5c
Martín Araya <mbozo@uc.cl>
2024-05-15 18:16:01 -0400

    Testing deploy

--- routes/route.py
+++ routes/route.py
@@ -6,7 +6,7 @@ from config.database import discountsTable
 
 router = APIRouter()
 
-@router.get("/", response_model=list[Discount])
+@router.get("/hello", response_model=list[Discount])
 async def get_discounts(page: int = 1, count: int = 25, category: Optional[str] = None):
     offset = (page - 1) * count
     query = {}
