62fff84f35ff2ba64d3e8148652d5f1a6e34c19f
Martín Araya <mbozo@uc.cl>
2024-06-19 21:25:02 -0400

    mm y ahiSimplificado

--- routes/route.py
+++ routes/route.py
@@ -25,18 +25,22 @@ async def get_discounts(page: int = 1, count: int = 25, category: Optional[str] 
 
 @router.post("/discounts", response_model=Discount, status_code=status.HTTP_201_CREATED)
 async def create_discount(discount: Discount):
-    # Convertir Discount a diccionario
-    discount_dict = discount.dict(by_alias=True)
-    discount_dict["id"] = ObjectId()
     # Buscar el ID de la categoría en la base de datos
     category_doc = categoriesTable.find_one({"categoryName": discount.category})
     if category_doc is None:
         raise HTTPException(status_code=400, detail="Invalid category")
 
-    discount_dict["category"] = str(category_doc["_id"])
+    discount = {
+        "url": discount.url,
+        "local": discount.local,
+        "discount": discount.discount,
+        "description": discount.description,
+        "category": str(category_doc["_id"]),
+        "expiration": discount.expiration,
+        "days": discount.days
+    }
 
-    # Insertar en la base de datos
-    new_discount = discountsTable.insert_one(discount_dict)
+    new_discount = discountsTable.insert_one(discount)
     created_discount = discountsTable.find_one({"_id": new_discount.inserted_id})
 
     if created_discount is None:
