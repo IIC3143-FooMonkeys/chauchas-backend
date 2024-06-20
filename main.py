7b4a517a741ec1a3d7ea7a6a2c80c16a7b871457
donjavi-538 <javier.v.armijo@uc.cl>
2024-06-20 18:18:32 -0400

    chore: moduralize router - banks

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from routes.route import router
 from routes.categories import router as categories_router
+from routes.discounts import router as discounts_router
+from routes.banks import router as banks_router
 import uvicorn
 from fastapi import FastAPI
 from test_database.insertions import insert_tests
@@ -7,6 +9,8 @@ from test_database.insertions import insert_tests
 app = FastAPI()
 app.include_router(router)
 app.include_router(categories_router)
+app.include_router(discounts_router)
+app.include_router(banks_router)
 
 if __name__ == '__main__':
     #insert_tests()
--- routes/route.py
+++ routes/route.py
@@ -12,45 +12,6 @@ router = APIRouter()
 def read_root():
     return {"message": "Hello World"}
 
-# ROUTES FOR BANKS #
-
-@router.get("/banks", response_model=List[Bank])
-async def get_banks(page: int = 1, count: int = 25):
-    offset = (page - 1) * count
-    query = {}
-
-    banks = list(banksTable.find(query).skip(offset).limit(count))
-    return bankEntities(banks)
-
-@router.get("/banks/{id}", response_model=Bank)
-async def read_bank(id: str):
-    if (banks := banksTable.find_one({"_id": ObjectId(id)})) is not None:
-        return bankEntity(banks)
-    raise HTTPException(status_code=404, detail=f"Bank with id {id} not found")
-
-@router.post("/banks", response_model=Bank, status_code=status.HTTP_201_CREATED)
-async def create_bank(bank: Bank):
-    bank_dict = bank.model_dump()
-    bank_dict['_id'] = ObjectId()
-    bank_dict['id'] = ObjectId()
-    banksTable.insert_one(bankEntity(bank_dict))
-    return bankEntity(bank_dict)
-
-@router.put("/banks/{id}", response_model=Bank)
-async def update_bank(id: str, bank: Bank):
-    if banksTable.find_one({"_id": ObjectId(id)}) is not None:
-        banksTable.update_one({"_id": ObjectId(id)}, {"$set": bank.model_dump()})
-        updated_bank = banksTable.find_one({"_id": ObjectId(id)})
-        return bankEntity(updated_bank)
-    raise HTTPException(status_code=404, detail=f"Bank with id {id} not found")
-
-@router.delete("/banks/{id}", response_model=Bank)
-async def delete_bank(id: str):
-    if (bank := banksTable.find_one({"_id": ObjectId(id)})) is not None:
-        banksTable.delete_one({"_id": ObjectId(id)})
-        return bankEntity(bank)
-    raise HTTPException(status_code=404, detail=f"Bank with id {id} not found")
-
 # ROUTES FOR CARDS #
 
 @router.get("/cards", response_model=List[Card])
