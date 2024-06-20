74e8dd01888f48edcf7d2ab5ed86a28895d7ee58
Martín Araya <mbozo@uc.cl>
2024-06-19 20:09:37 -0400

    Cambios en los modelos test 1

--- models/todos.py
+++ models/todos.py
@@ -2,8 +2,21 @@ from pydantic import BaseModel
 import datetime as d
 class Discount(BaseModel): #Modificar en base al E-R
     url: str
-    title: str
+    local: str
     discount: int
     description: str
     category: str
     expiration: d.datetime
+    days: str
+
+
+class Bank(BaseModel):
+    name: str
+
+class Card(BaseModel):
+    bankId: str
+    cardType: str
+    bank: str
+
+class Category(BaseModel):
+    categoryName: str
