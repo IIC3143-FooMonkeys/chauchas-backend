2cca3c5b83ac52fe1855254f6168db010337da8a
Martín Araya <mbozo@uc.cl>
2024-06-20 18:31:21 -0400

    eso pfaltaba pue

--- routes/discounts.py
+++ routes/discounts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from typing import Optional, List
 from models.todos import Discount
 from schema.schema import discountEntity, discountEntities
@@ -7,16 +7,21 @@ from bson import ObjectId
 
 router = APIRouter()
 
+
 @router.get("/discounts", response_model=List[Discount])
-async def get_discounts(page: int = 1, count: int = 25, category: Optional[str] = None):
+async def get_discounts(
+        page: int = 1,
+        count: int = 25,
+        category: Optional[str] = Query(None, description="Category ID to filter discounts")
+):
     offset = (page - 1) * count
     query = {}
+
     if category:
         query["category"] = category
 
     discounts = list(discountsTable.find(query).skip(offset).limit(count))
     return discountEntities(discounts)
-
 @router.get("/discounts/{id}", response_model=Discount)
 async def read_discount(id: str):
     if (discount := discountsTable.find_one({"_id": ObjectId(id)})) is not None:
