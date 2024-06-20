0e3f8459af9deca334b4d98e682897c52d859829
donjavi-538 <javier.v.armijo@uc.cl>
2024-06-20 18:22:15 -0400

    chore: moduralize router - cards

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from routes.route import router
 from routes.categories import router as categories_router
 from routes.discounts import router as discounts_router
 from routes.banks import router as banks_router
+from routes.cards import router as cards_router
 import uvicorn
 from fastapi import FastAPI
 from test_database.insertions import insert_tests
@@ -11,6 +12,7 @@ app.include_router(router)
 app.include_router(categories_router)
 app.include_router(discounts_router)
 app.include_router(banks_router)
+app.include_router(cards_router)
 
 if __name__ == '__main__':
     #insert_tests()
--- routes/route.py
+++ routes/route.py
@@ -12,59 +12,6 @@ router = APIRouter()
 def read_root():
     return {"message": "Hello World"}
 
-# ROUTES FOR CARDS #
-
-@router.get("/cards", response_model=List[Card])
-async def get_cards(page: int = 1, count: int = 25):
-    offset = (page - 1) * count
-    query = {}
-
-    cards = list(cardsTable.find(query).skip(offset).limit(count))
-    return cardEntities(cards)
-
-@router.get("/cards/{id}", response_model=Card)
-async def read_card(id: str):
-    if (cards := cardsTable.find_one({"_id": ObjectId(id)})) is not None:
-        return cardEntity(cards)
-    raise HTTPException(status_code=404, detail=f"Card with id {id} not found")
-
-@router.get("/cards/by-bank", response_model=List[Card])
-async def get_cards_by_bank(bankId: str, page: int = 1, count: int = 25):
-    offset = (page - 1) * count
-    query = {"bankId": ObjectId(bankId)}
-
-    cards = list(cardsTable.find(query).skip(offset).limit(count))
-    if not cards:
-        raise HTTPException(status_code=404, detail=f"No cards found for the bank {bankId}")
-    return cardEntities(cards)
-
-@router.post("/cards", response_model=Card, status_code=status.HTTP_201_CREATED)
-async def create_card(card: Card):
-    bank_doc = banksTable.find_one({"name": str(card.bankName)})
-    if bank_doc is None:
-        raise HTTPException(status_code=400, detail="Invalid bank")
-    card_dict = card.model_dump()
-    card_dict['bankId'] = str(bank_doc["_id"])
-    card_dict['_id'] = ObjectId()
-    card_dict['id'] = ObjectId()
-    cardsTable.insert_one(cardEntity(card_dict))
-    return cardEntity(card_dict)
-
-@router.put("/cards/{id}", response_model=Card)
-async def update_card(id: str, card: Card):
-    if cardsTable.find_one({"_id": ObjectId(id)}) is not None:
-        cardsTable.update_one({"_id": ObjectId(id)}, {"$set": card.model_dump()})
-        updated_card = cardsTable.find_one({"_id": ObjectId(id)})
-        return cardEntity(updated_card)
-    raise HTTPException(status_code=404, detail=f"Card with id {id} not found")
-
-@router.delete("/cards/{id}", response_model=Card)
-async def delete_card(id: str):
-    if (card := cardsTable.find_one({"_id": ObjectId(id)})) is not None:
-        cardsTable.delete_one({"_id": ObjectId(id)})
-        return cardEntity(card)
-    raise HTTPException(status_code=404, detail=f"Card with id {id} not found")
-
 # ROUTES FOR USERS #
 
 @router.get("/users", response_model=List[User])
