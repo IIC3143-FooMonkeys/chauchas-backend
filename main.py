f16ae5c79c8d10a759d501b1e1d9b8a7907aa34f
Martín Araya <mbozo@uc.cl>
2024-06-20 21:51:10 -0400

    fix: port changes for 443

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ app.include_router(users_router)
 
 if __name__ == '__main__':
     #insert_tests()
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
